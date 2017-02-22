3594cd48035a5fea17aba649a2c7425da0130fed
Diego van Dyk <diegolearnstocode@gmail.com>
2017-02-21 18:14:56 -0800

    Created Seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206235328) do
+ActiveRecord::Schema.define(version: 20170222014144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20170206235328) do
     t.string   "username"
     t.string   "city"
     t.string   "country"
+    t.string   "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |user|
+  user = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    username: Faker::Pokemon.name.downcase,
+    password: 111111,
+    avatar: Faker::Avatar.image,
+    city: Faker::Address.city,
+    country: Faker::Address.country
+    )
+  user.save
+end
+
+Category.new(name: 'Cat 1')
+Category.new(name: 'Cat 2')
+Category.new(name: 'Cat 3')
+Category.new(name: 'Cat 4')
+Category.new(name: 'Cat 5')
+
+PriceRange.new(name: 'Low')
+PriceRange.new(name: 'Medium')
+PriceRange.new(name: 'High')
+
+10.times do |product|
+  product = Product.new(
+    user_id: User.pluck(:id).sample,
+    image: Faker::Placeholdit.image("50x50"),
+    title: Faker::Hipster.sentence(3),
+    category_id: Category.pluck(:id).sample,
+    price_range_id: PriceRange.pluck(:id).sample,
+    description: Faker::Hipster.sentence(8),
+    )
+  product.save
+  5.times do |find|
+    find = ProductFind.new(
+      user_id: User.pluck(:id).sample,
+      product_id: product.id,
+      image: Faker::Placeholdit.image("50x50"),
+      title: Faker::Hipster.sentence(3),
+      url: 'http://google.com',
+      price: 100,
+      description: Faker::Hipster.sentence(8),
+      )
+    find.save
+  end
+end
+
+
